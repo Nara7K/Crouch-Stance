4836cee822cd417eca02ceb8c77803a7d0cf914f
Nara <35076718+Nara7K@users.noreply.github.com>
2022-08-01 11:06:42 -0500

    Add third mode, "Toggle + Sprint Only"
    Add third mode, "Toggle + Sprint Only"
    
    This mode keeps crouch toggled unless you hold the sprint key.

--- lua/autorun/stancecrouch.lua
+++ lua/autorun/stancecrouch.lua
@@ -8,10 +8,13 @@ if CLIENT then
 	concommand.Add("crouchstance_mode", function (ply, cmd)
 		if StanceCrouchMode == 0 then
 			StanceCrouchMode = 1
-			ply:PrintMessage(HUD_PRINTCENTER, "Crouch Stance = Toggle")
-		elseif StanceCrouchMode == 1 then
-			StanceCrouchMode = 0
 			ply:PrintMessage(HUD_PRINTCENTER, "Crouch Stance = Toggle + Run")
+		elseif StanceCrouchMode == 1 then
+			StanceCrouchMode = 2
+			ply:PrintMessage(HUD_PRINTCENTER, "Crouch Stance = Toggle + Sprint Only")
+		elseif StanceCrouchMode == 2 then
+			StanceCrouchMode = 0
+			ply:PrintMessage(HUD_PRINTCENTER, "Crouch Stance = Toggle")
 		end
 	end )
 
@@ -21,13 +24,19 @@ if CLIENT then
 			if not ply:IsPlayer() or !ply:Alive() then return end
 			
 			if StanceCrouchMode == 0 then
+				cmd:AddKey(IN_DUCK)
+			elseif StanceCrouchMode == 1 then
 				if ((cmd:GetForwardMove() > 0) or (cmd:GetForwardMove() < 0) or (cmd:GetSideMove() > 0) or (cmd:GetSideMove() < 0)) then
 					return
 				else
 					cmd:AddKey(IN_DUCK)
 				end
-			elseif StanceCrouchMode == 1 then
-				cmd:AddKey(IN_DUCK)
+			elseif StanceCrouchMode == 2 then
+				if ply:KeyDown( 131072 ) then
+					return
+				else
+					cmd:AddKey(IN_DUCK)
+				end
 			end
 		else
 			return
